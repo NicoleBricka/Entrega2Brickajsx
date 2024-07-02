8bfb8e0dafea31066b9570cc989573866c0aaf77
Nicole Bricka <brickanicole@gmail.com>
2024-07-02 01:52:03 -0300

    modificaciones

--- entrega1brickajsx/src/App.jsx
+++ entrega1brickajsx/src/App.jsx
@@ -1,12 +1,14 @@
 
 import React from "react";
 import NavBar from "./componentes/NavBar"; 
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 
 
 function App() {
     return (
-        <div>
+        <div >
             <NavBar/>
+            <ItemListContainer greeting={"Bienvenidos"}/>
         </div>
     );
 }
--- entrega1brickajsx/src/componentes/NavBar.jsx
+++ entrega1brickajsx/src/componentes/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import './NavBar.css'; 
-import CartWidget from "./CartWidget/CartWidget";
+import "./NavBar.css"; 
+import CartWidget from "./CartWidget./CartWidget./componentes"
 
 function NavBar() {
     return (
